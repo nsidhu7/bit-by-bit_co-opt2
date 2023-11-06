cc06e3f7379d077b166df6a5bd0c099e4dffe492
nsidhu7 <132525478+nsidhu7@users.noreply.github.com>
2023-11-06 14:26:30 -0600

    Updating code

--- app/src/main/java/com/mobile/coopt2/MainActivity.kt
+++ app/src/main/java/com/mobile/coopt2/MainActivity.kt
@@ -3,13 +3,36 @@ package com.mobile.coopt2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mobile.coopt2.ui.theme.Coopt2Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +45,55 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun NavDrawer(){
+    ModalNavigationDrawer(drawerContent = {
+        ModalDrawerSheet {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Coopt2Theme {
-        Greeting("Android")
+            Column(Modifier.fillMaxSize(), verticalArrangement = Arrangement.spacedBy(12.dp)) {
+
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .background(Color(0xfffc107)),
+                    contentAlignment = Alignment.Center
+                ){
+                    Column(Modifier.wrapContentSize(),
+                        verticalArrangement = Arrangement.SpaceAround,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(painter = painterResource(id = R.drawable.profile), contentDescription = "profile pic",
+                            modifier = Modifier
+                                .size(130.dp)
+                                .clip(CircleShape)
+                        )
+                        Text(text = "Name",
+                            Modifier
+                                .fillMaxWidth()
+                                .padding(top = 16.dp),
+                            fontSize = 22.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    }
+                    Divider(
+                        Modifier.align(Alignment.BottomCenter), thickness = 1.dp,
+                        Color.DarkGray
+                    )
+
+                }
+
+            }
+        }
+    }) {
+
     }
+
 }
